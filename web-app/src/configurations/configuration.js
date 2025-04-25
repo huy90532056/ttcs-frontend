3f7ba910d94bb2d21ab9f283070588d56cbbd781
huy90532056 <huytruong90532056@gmail.com>
2025-04-26 06:02:33 +0700

    update

---
+++ web-app/src/configurations/configuration.js
@@ -0,0 +1,6 @@
+export const OAuthConfig = {
+  clientId:
+    "682455445522-5afok8i6fvd1f1703mpp75n8qh9cl5hi.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
---
+++ web-app/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../components/Login";
+import Home from "../components/Home";
+import Authenticate from "../components/Authenticate";
+import Header from "../components/header/Header";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/authenticate" element={<Authenticate />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
